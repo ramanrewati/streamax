6f083e4668d4f714b0c475e6d6d5ab85b2a0a88b
ramanrewati <ramanrewatiboss@gmail.com>
2025-03-04 17:24:36 +0530

    fck

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const manifestUri = "https://otte.live.cf.ww.aiv-cdn.net/pdx-nitro/live/clients/dash/enc/rkyxparx1t/out/v1/fe9782633a364a6a84c9410f26d9b2c4/cenc.mpd";
+    var player = videojs("video", {
+        techOrder: ["html5"],
+        controls: true,
+        autoplay: true,
+        fluid: true,  // Responsive design
+        sources: [{
+            src: "https://otte.live.cf.ww.aiv-cdn.net/pdx-nitro/live/clients/dash/enc/rkyxparx1t/out/v1/fe9782633a364a6a84c9410f26d9b2c4/cenc.mpd",
+            type: "application/dash+xml"
+        }]
+    });
 
-    const clearkeyConfig = {
-        "org.w3.clearkey": {
-            "clearkeys": {
-                "494c5ddd4a2b9c38da05e053c0fd6d3f": "ad1ff8c72fc6fefbce716609da9347cf"
+    // Enable DRM
+    player.ready(function () {
+        this.eme();  // Activate DRM plugin
+        this.src({
+            src: "https://otte.live.cf.ww.aiv-cdn.net/pdx-nitro/live/clients/dash/enc/rkyxparx1t/out/v1/fe9782633a364a6a84c9410f26d9b2c4/cenc.mpd",
+            type: "application/dash+xml",
+            keySystems: {
+                "org.w3.clearkey": {
+                    keys: {
+                        "494c5ddd4a2b9c38da05e053c0fd6d3f": "ad1ff8c72fc6fefbce716609da9347cf"
+                    }
+                }
             }
-        }
-    };
-
-    const video = document.getElementById("video");
-    const player = dashjs.MediaPlayer().create();
-
-    player.initialize(video, manifestUri, true);
-    player.setProtectionData(clearkeyConfig);
+        });
+    });
 });
